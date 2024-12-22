e8245ef6f213f2cd7188851b5ab58fcafeea01f7
RakeshCharpe <rakeshcharpe99@gmail.com>
2024-12-23 01:51:53 +0530

    Learn insensitive and concur_updatable

--- src/main/java/org/example/ResultSetDemo.java
+++ src/main/java/org/example/ResultSetDemo.java
@@ -1,21 +1,16 @@
 package org.example;
 
-
-import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class App
-{
+public class ResultSetDemo {
     public static String LOAD_DRIVER="com.mysql.cj.jdbc.Driver";
     public static String URL="jdbc:mysql://localhost:3306/rakesh";
     public static String PASSWORD="root";
     public static String USERNAME="root";
-
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         try {
             //LOAD DRIVER
             Class.forName(LOAD_DRIVER);
@@ -23,25 +18,33 @@ public class App
             //MAKING CONNECTION
             Connection connection =  DriverManager.getConnection(URL,USERNAME,PASSWORD);
 
-            //CREATE STATEMENT
-            Statement statement = connection.createStatement();
+           //With the help of TYPE_SCROLL_INSENSITIVE , CONCUR_UPDATABLE we get the feature to
+            // access resultset directly
+
+            Statement statement = connection.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE
+            );
 
             //CREATE QUERY
             String query = "select * from demo";
 
             ResultSet resultSet = statement.executeQuery(query);
 
-            while(resultSet.next()){
-                int id =  resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                System.out.println("ID: "+ id +"name: "+name);
-            }
+           resultSet.first();
+           System.out.println("Id: "+resultSet.getInt("id"));
+           System.out.println("Name: "+resultSet.getString("name"));
+
+            resultSet.last();
+            System.out.println("Id: "+resultSet.getInt("id"));
+            System.out.println("Name: "+resultSet.getString("name"));
+            resultSet.updateString("name","virat");
+            resultSet.updateRow();
+
+
             connection.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
-
-
     }
 }
